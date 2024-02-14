6dafc37937faa7a50e4f6132d24cc98dc53a60cf
VeeK9 <vytautas.kakaras@gmail.com>
2024-02-14 10:06:19 +0200

    task 1

--- javascript - 02.14 - atsiskaitymas/Task 1/script.js
+++ javascript - 02.14 - atsiskaitymas/Task 1/script.js
@@ -8,3 +8,29 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const kgs = document.createElement('h1');
+const lbs = document.createElement('h1');
+const grams = document.createElement('h1');
+const oz = document.createElement('h1');
+document.querySelector('#output').append(kgs, lbs, grams, oz);
+
+document.querySelector('.wrapper > form').addEventListener('submit', e => {
+    e.preventDefault();
+    const kgsInput = parseFloat(e.target.elements.search.value);
+    console.log(kgsInput)
+    
+    if(kgsInput <= 0){
+        kgs.textContent = 'Please type in a positive number.';
+        lbs.textContent = ``;
+        grams.textContent = ``;
+        oz.textContent = ``;
+    } else {
+        kgs.textContent = `${kgsInput} kg is:`
+        lbs.textContent = `${(kgsInput*2.2046).toFixed(2)} lbs`;
+        grams.textContent = `${(kgsInput/0.001).toFixed(2)} grams`;
+        oz.textContent = `${(kgsInput*35.274).toFixed(2)} ounces`;
+    }
+
+    e.target.reset();
+})
+
