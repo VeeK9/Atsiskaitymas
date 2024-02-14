cdf520a77ad6a5fb0f1e0544d09a7d34bc404f3c
VeeK9 <vytautas.kakaras@gmail.com>
2024-02-14 13:07:55 +0200

    task 8

--- javascript - 02.14 - atsiskaitymas/Task 8/script.js
+++ javascript - 02.14 - atsiskaitymas/Task 8/script.js
@@ -5,3 +5,18 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    sum(a, b){
+        return a + b;
+    }
+    subtraction(a, b){
+        return a - b;
+    }
+    multiplication(a, b){
+        return a * b;
+    }
+    division(a, b){
+        return a / b
+    }
+};
