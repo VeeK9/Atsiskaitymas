98201c91eb6e96275100070238fa5cac32390c6c
VeeK9 <vytautas.kakaras@gmail.com>
2024-02-14 12:44:41 +0200

    task 3

--- javascript - 02.14 - atsiskaitymas/Task 3/script.js
+++ javascript - 02.14 - atsiskaitymas/Task 3/script.js
@@ -11,3 +11,29 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+document.querySelector('#btn').addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector('#message').remove();
+    document.querySelector('#output').style.justifyContent = 'center';
+    fetch(ENDPOINT)
+        .then(res => res.json())
+        .then(users => {
+            users.forEach(user => {
+                const userCard = document.createElement('div');
+                userCard.classList.add('userCard');
+
+                const avatar = document.createElement('img');
+                avatar.setAttribute('src', user.avatar_url);
+                avatar.setAttribute('alt', `${user.login}'s avatar`);
+
+                const login = document.createElement('h3');
+                const loginText = document.createTextNode(user.login);
+                login.appendChild(loginText);
+
+                userCard.append(avatar,login);
+    
+                document.querySelector('#output').appendChild(userCard);
+            })
+        })
+}, {once:true})
